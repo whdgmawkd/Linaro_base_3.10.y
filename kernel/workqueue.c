6c32d0cc1469f091b642a4418cfcc24b266c624b
Lai Jiangshan <laijs@cn.fujitsu.com>
2014-06-03 15:33:27 +0800

    workqueue: clear POOL_DISASSOCIATED in rebind_workers()
    a9ab775bcadf ("workqueue: directly restore CPU affinity of workers
    from CPU_ONLINE") moved pool locking into rebind_workers() but left
    "pool->flags &= ~POOL_DISASSOCIATED" in workqueue_cpu_up_callback().
    
    There is nothing necessarily wrong with it, but there is no benefit
    either.  Let's move it into rebind_workers() and achieve the following
    benefits:
    
      1) better readability, POOL_DISASSOCIATED is cleared in rebind_workers()
         as expected.
    
      2) we can guarantee that, when POOL_DISASSOCIATED is clear, the
         running workers of the pool are on the local CPU (pool->cpu).
    
    tj: Minor description update.
    
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -4665,6 +4665,7 @@ static void rebind_workers(struct worker_pool *pool)
 						  pool->attrs->cpumask) < 0);
 
 	spin_lock_irq(&pool->lock);
+	pool->flags &= ~POOL_DISASSOCIATED;
 
 	for_each_pool_worker(worker, wi, pool) {
 		unsigned int worker_flags = worker->flags;
@@ -4768,10 +4769,6 @@ static int workqueue_cpu_up_callback(struct notifier_block *nfb,
 			mutex_lock(&pool->manager_mutex);
 
 			if (pool->cpu == cpu) {
-				spin_lock_irq(&pool->lock);
-				pool->flags &= ~POOL_DISASSOCIATED;
-				spin_unlock_irq(&pool->lock);
-
 				rebind_workers(pool);
 			} else if (pool->cpu < 0) {
 				restore_unbound_workers_cpumask(pool, cpu);
