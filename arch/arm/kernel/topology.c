cbd35edf7dcdead42d51e7c688d64155d5929cef
Mark Brown <broonie@linaro.org>
2016-02-25 22:17:41 +0900

    ARM: 7920/1: topology: Staticise non-exported symbols
    These symbols are only referenced in this source file so can be made
    static, and the efficiency table is constant data so can be declared as
    such.  This avoids polluting the global namespace and fixes warnings
    from sparse.
    
    The function arch_scale_freq_power() is still not prototyped or static,
    this is a separate issue as this is overriding a weak symbol from the
    scheduler which neglects to provide a prototype.
    
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    (cherry picked from commit 145bc292dce9dbdface2acf1e7e1f175729fb5fb)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -70,6 +70,7 @@ struct cpu_efficiency {
  * use the default SCHED_POWER_SCALE value for cpu_scale.
  */
 struct cpu_efficiency table_efficiency[] = {
+static const struct cpu_efficiency table_efficiency[] = {
 #ifdef CONFIG_SOC_EXYNOS5433
 	{"arm,cortex-a15", 3575},
 #else
@@ -79,10 +80,10 @@ struct cpu_efficiency table_efficiency[] = {
 	{NULL, },
 };
 
-unsigned long *__cpu_capacity;
+static unsigned long *__cpu_capacity;
 #define cpu_capacity(cpu)	__cpu_capacity[cpu]
 
-unsigned long middle_capacity = 1;
+static unsigned long middle_capacity = 1;
 
 /*
  * Iterate all CPUs' descriptor in DT and compute the efficiency
@@ -94,7 +95,7 @@ unsigned long middle_capacity = 1;
  */
 static void __init parse_dt_topology(void)
 {
-	struct cpu_efficiency *cpu_eff;
+	const struct cpu_efficiency *cpu_eff;
 	struct device_node *cn = NULL;
 	unsigned long min_capacity = (unsigned long)(-1);
 	unsigned long max_capacity = 0;
@@ -163,7 +164,7 @@ static void __init parse_dt_topology(void)
  * boot. The update of all CPUs is in O(n^2) for heteregeneous system but the
  * function returns directly for SMP system.
  */
-void update_cpu_power(unsigned int cpu)
+static void update_cpu_power(unsigned int cpu)
 {
 	if (!cpu_capacity(cpu))
 		return;
@@ -190,7 +191,7 @@ const struct cpumask *cpu_coregroup_mask(int cpu)
 	return &cpu_topology[cpu].core_sibling;
 }
 
-void update_siblings_masks(unsigned int cpuid)
+static void update_siblings_masks(unsigned int cpuid)
 {
 	struct cputopo_arm *cpu_topo, *cpuid_topo = &cpu_topology[cpuid];
 	int cpu;
