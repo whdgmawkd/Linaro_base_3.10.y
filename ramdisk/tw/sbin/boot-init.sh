fcfce9bd2b7f88316c5980b025875aa25f399963
DQ <dqstyle@gmail.com>
2016-04-01 07:20:48 +0900

    ramdisk/synapse: disabled synapse interface
    Signed-off-by: DQ <dqstyle@gmail.com>

--- ramdisk/tw/sbin/boot-init.sh
+++ ramdisk/tw/sbin/boot-init.sh
@@ -218,10 +218,10 @@ if [ $INS_LAST -eq 1 ]; then
 	fi
 fi
 
-/sbin/uci reset
-/sbin/uci
+#/sbin/uci reset
+#/sbin/uci
 
-source /sbin/synapse_loader.sh
+#source /sbin/synapse_loader.sh
 
 echo init.d script start >> /data/PRIME-Kernel/kernel.log
 echo - excecuted on $(date +"%Y-%d-%m %r") >> /data/PRIME-Kernel/kernel.log
